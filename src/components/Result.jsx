8e134aec8ef1dfba1a31519ec72eae16d8362d92
Gabrielle <gabriellemarie@knights.ucf.edu>
2025-03-28 11:35:47 -0700

    more css adjustments

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Result(props) {
   return (
     <div className='d-flex justify-content-center flex-column'>
-       <p className='just-another-hand-regular mt-3'> You got {props.totalCorrectAnswers}/10 correct! {props.totalCorrectAnswers >= 7 ? "Yay!" : "Better luck next time."}</p>
+       <p className='intro-text mt-3'> You got {props.totalCorrectAnswers}/10 correct! {props.totalCorrectAnswers >= 7 ? "Yay!" : "Better luck next time."}</p>
        <button className='intro-btn' onClick={() => {props.resetQuiz()}}>RETRY</button>
         
     </div>
